141d38840f1bebf2f10d7fc10e60f13c17e02665
Anthony Davidson <anthony.davidson@canberra.edu.au>
2021-08-24 23:51:06 +1000

    sorting actions hopefully

--- checkexternal.R
+++ checkexternal.R
@@ -1,41 +1,82 @@
-name: newone
+library(rvest)
+library(RSelenium)
+library(tidyverse)
+library(DescTools)
+library(knitr)
+library(arsenal)
+library(rgdal)
+library(dplyr)
 
-# Controls when the action will run.
-on:
-  schedule:
-    - cron:  '30 5,17 * * *'
 
-  # Allows you to run this workflow manually from the Actions tab
-  workflow_dispatch:
+#####function 1 #####################
+fixgeo <- function(search,  lat, lon, column="Exposure.Location",tt=tab3) {
   
-jobs: 
-  autoscrape:
-    # The type of runner that the job will run on
-    runs-on: macos-latest
+  ii <- NA
+  ii <- grep(search,tt[,which(column==colnames(tab3))])
+  if(length(ii>0)) {
+    for (c in 1:length(ii)){
+      tt[ii[c],"lat"] <-lat
+      tt[ii[c],"lon"] <- lon
+    }
+  }
+  return(tt)
+}
 
-    # Load repo and install R
-    steps:
-    - uses: actions/checkout@v2
-    - uses: r-lib/actions/setup-r@master
+#grab from website
+es <- read_html("https://www.covid19.act.gov.au/act-status-and-response/act-covid-19-exposure-locations", )
 
-    # Set-up R
-    - name: Install packages
-      run: |
-        R -e 'install.packages("tidyverse")'
-        R -e 'install.packages("janitor")'
-        R -e 'install.packages("rvest")'
-    # Run R script
-    - name: Scrape
-      run: Rscript nifty50_scrapping.R
-      
- # Add new files in data folder, commit along with other modified files, push
-    - name: Commit files
-      run: |
-        git config --local user.name actions-user
-        git config --local user.email "actions@github.com"
-        git add data/*
-        git commit -am "GH ACTION Headlines $(date)"
-        git push origin main
-      env:
-        REPO_KEY: ${{secrets.GITHUB_TOKEN}}
-        username: github-actions
+#check update
+ll <- es %>%
+  html_nodes("strong") %>%
+  html_text()
+index <- grep("Page last updated:",ll)
+dummy <- ll[index]
+lup <- dummy
+lu <- substr(strsplit(dummy,"updated:")[[1]][2],2,100)
+lu <- gsub(" ", "_",lu)
+lu <- gsub(":","",lu)
+lu
+
+#check if there was an update....
+ff <- list.files("data/")
+wu <- grep(lu, ff)
+
+# if(length(wu)==0)
+if(length(wu)==0){
+  print("Check on github")
+} #loop end
+  #, "No stress. Nothing new on the ACT Health site")
+
+
+
+##############next step of code
+##### scrape covid exposure table from website
+#can this be done from saved html file?
+#or csv>
+
+
+
+#rD <- rsDriver(browser="firefox", port=4545L, verbose=FALSE)
+#remDr <- rD[["client"]]
+#remDr$navigate("https://www.covid19.act.gov.au/act-status-and-response/act-covid-19-exposure-locations")
+
+#Sys.sleep(5) # give the page time to fully load
+#html <- remDr$getPageSource()[[1]]
+
+#remDr$close()
+#rD$server$stop()
+#rm(rD)
+#gc()
+#necessary to stop the server...
+#system("taskkill /im java.exe /f", intern=FALSE, ignore.stdout=FALSE)
+#signals <- read_html(html)
+
+
+#tbls <- signals %>%
+#  html_nodes("table") %>%
+#  html_table(fill = TRUE)
+
+#tab3 <- data.frame(tbls)
+
+###save script here for reassessing
+#write.rds
