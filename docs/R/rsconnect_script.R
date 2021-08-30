b85ae9ee0c934924c677aa0a46b2ceff017b96a5
Anthony Davidson <anthony.davidson@canberra.edu.au>
2021-08-30 16:09:45 +1000

    site build up

--- docs/R/rsconnect_script.R
+++
@@ -1,7 +0,0 @@
-# https://raw.githubusercontent.com/ACT-covid-resources/covid_canberra/main/Covid_Exposure_ACT.rmd
-
-library(rsconnect)
-rsconnect::accountInfo(name = 'the-statistics-network-ssn')
-rsconnect::deployApp("index.Rmd")
-
-rsconnect::configureApp("index.Rmd")
